27fb7c98a2ffa5e727b2cf3449b2a8703e7e0e0f
Lajos Koszti <lajos@koszti.hu>
2024-12-27 19:58:06 +0100

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ajnasz/wol
+
+go 1.23.4
---
+++ wol.go
@@ -0,0 +1,81 @@
+package wol
+
+import (
+	"errors"
+	"fmt"
+	"net"
+)
+
+var ErrInvalidMACAddress = errors.New("Invalid MAC address")
+
+// MagicPacket represents the Wake-on-LAN packet structure
+type MagicPacket struct {
+	header  []byte
+	macAddr []byte
+}
+
+// NewMagicPacket creates a new Wake-on-LAN magic packet
+func NewMagicPacket(macAddr string) (*MagicPacket, error) {
+	// Parse MAC address
+	mac, err := net.ParseMAC(macAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(mac) != 6 {
+		return nil, ErrInvalidMACAddress
+	}
+
+	// Create magic packet structure
+	packet := &MagicPacket{
+		header:  make([]byte, 6),  // 6 bytes of 0xFF
+		macAddr: make([]byte, 96), // MAC address repeated 16 times
+	}
+
+	// Fill header with 0xFF
+	for i := 0; i < 6; i++ {
+		packet.header[i] = 0xFF
+	}
+
+	// Fill MAC address portion (16 repetitions)
+	for i := 0; i < 16; i++ {
+		copy(packet.macAddr[i*6:(i+1)*6], mac)
+	}
+
+	return packet, nil
+}
+
+// Bytes returns the complete magic packet as a byte slice
+func (mp *MagicPacket) Bytes() []byte {
+	packet := make([]byte, 102) // 6 + 96 bytes total
+	copy(packet[0:6], mp.header)
+	copy(packet[6:], mp.macAddr)
+	return packet
+}
+
+// SendPacket sends the magic packet to the specified address
+func SendPacket(macAddr, broadcastAddr string) error {
+	packet, err := NewMagicPacket(macAddr)
+	if err != nil {
+		return err
+	}
+
+	// Create UDP address
+	addr, err := net.ResolveUDPAddr("udp", broadcastAddr+":0")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Sending magic packet to", addr)
+
+	// Create UDP connection
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	// Send packet
+	_, err = conn.Write(packet.Bytes())
+	return err
+}
